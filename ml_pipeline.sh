b53d01a9c9598f0a36af2dff12ae0d845471cc43
Keerthi <keerthi.bachu@capgemini.com>
2020-03-12 11:34:59 +0000

    updated

--- ml_pipeline.sh
+++ ml_pipeline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #updated model, push to dvc and git  
-cd /app/continuous-intelligence && python src/test_script.py
+#cd /app/continuous-intelligence && python src/test_script.py
 
-#cd /app/continuous-intelligence && dvc repro model.pkl.dvc && dvc push model.pkl.dvc && git add model.pkl.dvc && git commit -m 'model updated' && git push https://$GIT_USER:$GIT_TOKEN@github.com/KeeBac/cd4ml-workshop-2.git
+cd /app/continuous-intelligence && dvc repro model.pkl.dvc && dvc push model.pkl.dvc && git add model.pkl.dvc && git commit -m 'model updated' && git push https://$GIT_USER:$GIT_TOKEN@github.com/KeeBac/cd4ml-workshop-2.git
